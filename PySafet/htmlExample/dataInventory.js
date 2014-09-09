365aad6e7020e4d489a5baa225a87977815dc043
Víctor Bravo Bravo <vbravo@seguridad01.(none)>
2014-09-09 16:30:54 -0430

    adding html Example with json

---
+++ PySafet/htmlExample/dataInventory.js
@@ -0,0 +1,3 @@
+dataInventory = [['27', 'Champu X', 'Registrado'], ['28', 'Champu Y', 'Registrado'], ['29', 'Limpiador de cutis', 'Registrado'], ['30', 'QuitaCuticula', 'Registrado'], ['31', 'Crema Humectante', 'Registrado'], ['32', 'Pa\xf1ales M', 'Registrado'], ['33', 'Pa\xf1ales G', 'Registrado'], ['34', 'Pa\xf1ales XXG Marca L', 'Registrado'], ['35', 'Crema de manos', 'Registrado'], ['36', 'Crema de pies', 'Registrado']]
+
+columnInventory = [{'sTitle': 'id'}, {'sTitle': 'nombre'}, {'sTitle': 'status'}]
